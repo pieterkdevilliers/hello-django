56e4cf636b7d1d7283fc08ea1fe98ae76c161ad4
pieterkdevilliers <pieterkdevilliers@me.com>
2022-10-25 13:24:00 +0000

    Add heroku host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pkdevilliers-django-todo.herokuapp.com']
 
 
 # Application definition
